3866cebc69b70e2e955620d8fdc9fe71be18ae7c
nekajcasa <maticko.music@gmail.com>
2023-09-09 11:31:25 +0200

    V1
    update

--- DataLoger.py
+++ DataLoger.py
@@ -13,36 +13,35 @@ import json
 import pandas as pd
 from sqlalchemy import create_engine
 
+
 class DL():
     def __init__(self):
         self.engine = create_engine("sqlite:///E:\\projekti\\trading\\test.db")
         self.endpoint = "wss://stream.binance.com:9443/ws"
-        self.ws = websocket.WebSocketApp(self.endpoint,on_message=self.on_message,
-                                         on_open=self.on_open,on_close=self.on_close)
+        self.ws = websocket.WebSocketApp(self.endpoint, on_message=self.on_message,
+                                         on_open=self.on_open, on_close=self.on_close)
         self.ws.run_forever()
-        
-        
-    def on_open(self,ws):
+
+    def on_open(self, ws):
         ws.send(json.dumps({"method": "SUBSCRIBE",
-                            "params":["btcusdt@ticker"],
+                            "params": ["btcusdt@ticker"],
                             "id": 1}))
-        
-    def on_close(self,ws):
+
+    def on_close(self, ws):
         ws.send(json.dumps({"method": "UNSUBSCRIBE",
                             "params": ["btcusdt@depth"],
                             "id": 312}))
 
-    def on_message(self,ws,message):
-        
+    def on_message(self, ws, message):
+
         out = json.loads(message)
-        
-        time = pd.to_datetime(out["E"],unit="ms")
+
+        time = pd.to_datetime(out["E"], unit="ms")
         price = float(out["c"])
         df = pd.DataFrame({"Time": time, "Price": price}, index=[0])
         naslov = str(time)[:11]
-        print(naslov,price)
-        df.to_sql("BTCUSDT",self.engine,if_exists="append",index=False)
+        df.to_sql(naslov, self.engine, if_exists="append", index=False)
+
 
 if __name__ == "__main__":
     DL()
-    
